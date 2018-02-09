0ee4653b3e9973f7d11b83d2a7102f51fc9fd356
Aakash Tripathi <aakasht05@gmail.com>
2018-02-08 22:59:08 -0500

    Completed Survey Form Assignment

---
+++ MEAN/03_Express/02_SurveyForm/server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function(request, response){
+    response.render("index");
+});
+
+app.post("/result", function(request, response){
+    response.render("results",{name: request.body.name, location: request.body.location, language: request.body.language, comment: request.body.comment} )
+});
+
+app.get("/goback", function(request, response){
+    response.redirect("/");
+});
+
+app.listen(8000, function(){
+    console.log("Server is running on port: 8000");
+});
